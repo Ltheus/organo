50e45fc8e69d26e79b49459f6fe8d59046867605
Ltheus <matheusglopes1@gmail.com>
2024-10-24 21:09:12 -0300

    Create banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Banner from "./components/Banner/Banner";
+import Banner from "./components/Banner";
+import TextInput from "./components/TextInput";
 
 function App() {
   return (
     <>
       <Banner />
+      <TextInput label="Nome" placeholder="Digite seu nome" />
+      <TextInput label="Cargo" placeholder="Digite seu cargo" />
+      <TextInput label="Imagem" placeholder="Digite o endereço da imagem" />
     </>
   );
 }
