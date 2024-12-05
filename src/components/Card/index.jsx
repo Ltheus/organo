022f7187977ca8443a0b3b8e422a1ff61d9e7fea
Ltheus <matheusglopes1@gmail.com>
2024-12-04 22:13:42 -0300

    Created Card component mock

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,17 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="header">
+        <img src="https://github.com/Ltheus.png" alt="eu" />
+      </div>
+      <div className="footer">
+        <h4>Matheus</h4>
+        <h5>Boiola</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
